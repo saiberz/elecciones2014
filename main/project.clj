d20dc38238da479f7ed08607339d66aa3ff860bf
FredyM <saiberz@hotmail.com>
2014-07-21 09:22:47 -0500

    resultados folder added

--- main/project.clj
+++ main/project.clj
@@ -1,6 +1,6 @@
-(defproject main "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject webdata "0.1.0-Beta"
+  :description "Elecciones Municipales 2014"
+  :url "https://github.com/saiberz/elecciones2014"
   :jvm-opts ["-Dphantomjs.binary.path=./phantomjs-1.9.1-linux-x86_64/bin/phantomjs"]
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
@@ -8,4 +8,6 @@
                  [clj-webdriver/clj-webdriver "0.6.0"]
                  [com.github.detro.ghostdriver/phantomjsdriver "1.0.3"]
                  [org.clojure/data.json "0.2.5"]]
+  ;:main webdata.core
+  ;:aot :all
   )
